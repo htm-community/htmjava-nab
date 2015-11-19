5aa9c2ab5abaf170721884b7629fa697e53da9aa
Fergal Byrne <fergalbyrnedublin@gmail.com>
2015-11-19 15:57:45 +0000

    Added htm.java as a dep

--- project.clj
+++ project.clj
@@ -3,7 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.numenta/htm.java "0.6.5"]]
   :main ^:skip-aot htmjava-nab.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                              ;; Disable signing releases deployed to this repo.
+                              ;; (Not recommended.)
+                              :sign-releases false
+                              ;; You can also set the policies for how to handle
+                              ;; :checksum failures to :fail, :warn, or :ignore.
+                              :checksum :fail
+                              ;; How often should this repository be checked for
+                              ;; snapshot updates? (:daily, :always, or :never)
+                              :update :daily}]])
+
